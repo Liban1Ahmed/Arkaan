09a52bb064cae2748e97f64a29a5733175757c24
Your Name <you@example.com>
2026-08-21 16:16:12 +0100

    Fix 500 error on unauthenticated requests - return proper 401

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,8 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'site-admin' => \App\Http\Middleware\SiteAdminMiddleware::class,
         ]);
+
+        $middleware->redirectGuestsTo(fn () => null);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
